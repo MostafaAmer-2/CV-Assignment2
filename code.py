920eb962bb6c837e70b452ae60696a7d30dcb49e
Karim ElGhandour <kghandour@live.com>
2019-11-21 01:10:48 +0200

    Q2 should be done

--- code.py
+++ code.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 
 def loadTrainingSet():
@@ -45,15 +46,53 @@ def perceptron():
 
     print(confusionMatrix)
 
+def naive():
+    images = loadTrainingSet()
+    
+    mews = np.zeros([10,784])
+    standard_div = np.zeros([10,784])
+    for c in range(10):
+        mew_c = np.zeros(784)
+        for i in range( c*240 , c*240+240):
+            mew_c = mew_c + np.true_divide(images[i,:-1],255)
+        mews[c] = np.true_divide(mew_c,240)
 
+    for c in range(10):
+        std_c = np.zeros(784)
+        for i in range( c*240 , c*240+240):
+            std_c = std_c + ( pow(np.true_divide(images[i,:-1],255) - mews[c],2) )
+        standard_div[c] = np.true_divide( std_c,240 )
 
+    return mews, standard_div
+    
+def naive_test(filepath):
+    mews, standard_div = naive()
+    img = plt.imread(filepath)
+    img2 = img.flatten()
+    img3 = np.append(img2, 1)
+    img3 = np.true_divide(img3[:-1],255)
 
+    probs = np.ones(10)
+    for c in range(10):
+        for i in range(784):
+            std_div = 0.1
+            if(standard_div[c,i]>0.1):
+                std_div = standard_div[c,i]
+            gaussian = gaussian_eq(img3[i], mews[c,i], std_div)
+            probs[c] = probs[c]*gaussian
+    
+    print(np.argmax(probs))
+
+    
+def gaussian_eq(n, mew, std_div):
+    return 1/math.sqrt(2*math.pi*std_div)*math.exp(- ( pow(n - mew,2) ) / ( 2*std_div ) )
 
 if __name__ == "__main__":
 
     # images = loadTrainingSet()
     # print(images[0, 10])
-    perceptron()
+    # perceptron()
+    naive_test("./Test/142.jpg")
     # print(configureInitialWeightMatrix())
 
 
